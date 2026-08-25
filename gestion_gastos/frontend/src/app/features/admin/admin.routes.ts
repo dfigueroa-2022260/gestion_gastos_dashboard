28eafa1ad7764976d2f526b8d8985daec880b913
dfigueroa-2022260 <dfigueroa-2022260@kinal.edu.gt>
2026-08-25 01:55:44 -0600

    refactor: se arreglo problema

--- gestion_gastos/frontend/src/app/features/admin/admin.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const ADMIN_ROUTES: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./admin-usuarios.component').then((m) => m.AdminUsuariosComponent),
-  },
-];
--- gestion_gastos/frontend/src/app/features/gastos/gastos.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const GASTOS_ROUTES: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./gastos-home.component').then((m) => m.GastosHomeComponent),
-  },
-];
